e122b616cdd2f3f8a6b8f04f8f6e48d50a136bfd
Scott Walker <scott.walker@puppet.com>
2019-04-04 17:59:08 +0100

    Configure logging

--- cmd/goplugin-google/main.go
+++ cmd/goplugin-google/main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
 	"github.com/lyraproj/terraform-bridge/cmd/goplugin-google/handler"
 )
+
+func init() {
+	// Configuring hclog like this allows Lyra to handle log levels automatically
+	hclog.DefaultOptions = &hclog.LoggerOptions{
+		Name:            "google",
+		Level:           hclog.LevelFromString(os.Getenv("LYRA_LOG_LEVEL")),
+		JSONFormat:      true,
+		IncludeLocation: false,
+		Output:          os.Stderr,
+	}
+}
 
 func main() {
 	handler.Start()
