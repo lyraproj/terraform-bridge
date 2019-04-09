3f211ec933142eb69b3aca717ca0f40d7f3f2975
Thomas Hallgren <thomas@tada.se>
2019-04-09 15:03:54 +0200

    Remove redundant hclog configuration
    This commit removes an old and faulty hclog configuration for the Aws
    plugin that replaced the proper one in main.go.

--- cmd/goplugin-aws/handler/start.go
+++ cmd/goplugin-aws/handler/start.go
@@ -1,13 +1,8 @@
 package handler
 
 import (
-	"sync"
-
-	"github.com/hashicorp/terraform/terraform"
-
-	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/lyraproj/issue/issue"
+	"github.com/hashicorp/terraform/terraform"
 	"github.com/lyraproj/pcore/pcore"
 	"github.com/lyraproj/pcore/px"
 	"github.com/lyraproj/servicesdk/grpc"
@@ -15,22 +10,10 @@ import (
 	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
 
-var once sync.Once
-
 const Namespace = `Aws`
 
 // Start this server running
 func Start() {
-	once.Do(func() {
-		hclog.DefaultOptions = &hclog.LoggerOptions{
-			Name:            Namespace,
-			Level:           hclog.Debug,
-			JSONFormat:      true,
-			IncludeLocation: false,
-		}
-		issue.IncludeStacktrace(true)
-	})
-
 	pcore.Do(func(c px.Context) {
 		s := bridge.CreateService(c, aws.Provider().(*schema.Provider), Namespace, &terraform.ResourceConfig{
 			Config: map[string]interface{}{
